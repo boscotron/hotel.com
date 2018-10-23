be4913d52ded0e9315abf4bc57a132a6b251b747
Juan Carlos M <Juan Carlos M>
2018-10-23 18:20:26 -0400

    inicio de proyecto

---
+++ templet/contacto.php
@@ -0,0 +1,104 @@
+
+    
+    <!-- SubHeader =============================================== -->
+    <section class="parallax-window" id="short" data-parallax="scroll" data-image-src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/sub_header_short_2.jpg" data-natural-width="1400" data-natural-height="350">
+        <div id="subheader">
+            <h1>Contacto</h1>
+		</div><!-- End subheader -->
+    </section><!-- End section -->
+    <!-- End SubHeader ============================================ -->
+	
+        
+    <div class="container_styled_1">
+        <div class="container margin_60">
+    <h2 class="main_title"><em></em>Bienvenido a Hotel-Restaurante Pozo Viejo <span></span></h2>
+    	<div class="row add_top_20">        
+        <div class="col-md-4">
+            	<div class="box_style_1">
+                <div class="box_contact">
+                    <i class="icon_set_1_icon-41"></i>
+                    <h4>Dirección</h4>
+                    <p>Federal Zaragoza Zacapoaxtla Km 5 s/n<br>Zacapoaxtla, Puebla, Mexico<br>
+					Route de Sablet 1023, Marseille</p>
+                    </div>
+                    <div class="box_contact">
+            	<i class="icon_set_1_icon-37"></i>
+                <h4>Obtener Ruta</h4>
+                <form action="https://www.google.com/maps/place/Hotel+Restaurant+Pozo+Viejo/@19.8437763,-97.5818189,19.13z/data=!4m21!1m15!4m14!1m6!1m2!1s0x85ce01b096693d3f:0x44d87c2a59fadae2!2sCalzada+del+Hueso+151,+El+Vergel+de+Coyoac%C3%A1n+(+Infonavit+El+Hueso),+INFONAVIT+el+Hueso,+04859+Ciudad+de+M%C3%A9xico,+D.F.,+M%C3%A9xico!2m2!1d-99.1354331!2d19.3077598!1m6!1m2!1s0x85da970f1e3180a7:0xa62b0c68427a16ac!2sHotel+Restaurant+Pozo+Viejo,+3+de+Mayo,+Calcahualco,+Puebla!2m2!1d-97.5815555!2d19.8438013!3m4!1s0x85da970f1e3180a7:0xa62b0c68427a16ac!8m2!3d19.8438013!4d-97.5815555" method="get" target="_blank">
+                	<div class="form-group">
+					<input type="text" name="saddr" placeholder="Ingresa tu punto de partida" class="form-control" />
+					<input type="hidden" name="daddr" value="Hotel Restaurant Pozo Viejo, Carretera Estatal Zaragoza-Zacapoaxtla Km. 5, 3 de Mayo, Calcahualco, Pue."/><!-- Write here your end point -->
+                    </div>
+                    <div class="form-group">
+					<button class="btn_1" type="submit" value="Get directions">Ver Ruta</button>
+                    </div>
+			</form>
+            </div>
+            </div>
+            </div>   
+            
+        	<div class="col-md-7 col-md-offset-1">
+            
+            <div id="message-contact"></div>
+				<form method="post" action="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>assets/contact.php" id="contactform">
+					<div class="row">
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label>Nombre</label>
+								<input type="text" class="form-control" id="name_contact" name="name_contact" placeholder="Ingresa tu Nombre">
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label>Apellido</label>
+								<input type="text" class="form-control" id="lastname_contact" name="lastname_contact" placeholder="Ingresa Apellido">
+							</div>
+						</div>
+					</div>
+					<!-- End row -->
+					<div class="row">
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label>Email</label>
+								<input type="email" id="email_contact" name="email_contact" class="form-control" placeholder="Ingresa Email">
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<div class="form-group">
+								<label>Teléfono</label>
+								<input type="text" id="phone_contact" name="phone_contact" class="form-control" placeholder="Ingresa número">
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="form-group">
+								<label>Mensaje</label>
+								<textarea id="message_contact" name="message_contact" class="form-control" placeholder="Escribe tu mensaje" style="height:150px;"></textarea>
+							</div>
+						</div>
+					</div>
+					<div class="row add_bottom_30">
+						<div class="col-md-6">
+                        	<div class="form-group">
+                                <label>Eres Humano</label>
+                                <input type="text" id="verify_contact" class=" form-control add_bottom_30" placeholder="Ingresa el total de: 3 + 1 =">
+                            </div>
+							<input type="submit" value="Enviar" class="btn_1" id="submit-contact">
+						</div>
+					</div>
+				</form>               
+            </div><!-- End col-md-8 -->    
+            
+             
+        </div><!-- End row -->
+    </div><!-- End Container --> </div><!-- End Container -->
+	<hr>
+    <div class="container">
+        <div class="container margin_60"> <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3752.9090468327117!2d-97.58155500000001!3d19.843801000000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa62b0c68427a16ac!2sHotel+Restaurant+Pozo+Viejo!5e0!3m2!1ses!2smx!4v1513290772496" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+	  </div></div>
+
+	<div id="toTop"></div><!-- Back to top button -->	
+	<div id="dtBox"></div><!-- End datepicker -->
+	
+	
---
+++ templet/inicio.php
@@ -0,0 +1,344 @@
+	<div class="row"><div class="carousel_in">
+      <div><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/slider/habitacion.jpg" alt=""><div class="caption"><h3>Comodidad</h3></div></div>
+      <div><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/slider/boda.jpg" alt=""><div class="caption"><h3>Bodas </h3></div></div>
+      <div><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/slider/comida.jpg" alt=""><div class="caption"><h3>Comida </h3></div></div>
+	  <div><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/slider/temazcal.jpg" alt=""><div class="caption"><h3>Temazcal</h3></div></div>
+      <div><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/slider/cumple.jpg" alt=""><div class="caption"><h3>Cumpleaños</h3></div></div>
+      <div><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/slider/piscina.jpg" alt=""><div class="caption"><h3>Piscina</h3></div></div>
+      <div><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/slider/visita.jpg" alt=""><div class="caption"><h3>Paseos</h3></div></div>
+      
+    </div> </div>   
+
+	
+	
+	
+	<div class="container_styled_1">
+        <div class="container ">
+            <div class="row">
+                <div class="col-lg-12">
+                    <figure class="room_pic">  
+					
+					
+					
+        <div class="col-lg-12">  
+            <div id="sub_content"> 
+                <div id="book_container">
+                    <form method="post" action="assets/check_avail_home.php" id="check_avail_home" autocomplete="off">
+                        <div id="group_1">
+                            <div id="container_1">
+                                <h4 style="color:white;">&nbsp;&nbsp;&nbsp;&nbsp;Llegada</h4>
+                            	<input class="startDate1 form-control datepick" type="text" data-field="date" data-startend="start" data-startendelem=".endDate1" readonly placeholder="Llegada" id="check_in" name="check_in">
+                            </div>
+                            <div id="container_2">
+                                <h4 style="color:white;">&nbsp;&nbsp;&nbsp;&nbsp;Salida</h4>
+                                 <input class="endDate1 form-control datepick" type="text" data-field="date" data-startend="end" data-startendelem=".startDate1" readonly placeholder="Salida" id="check_out" name="check_out">
+                            </div>
+                        </div><!-- End group_1 -->
+                        <div id="group_2">
+                            <div id="container_3">
+                                <h4 style="color:white;">&nbsp;&nbsp;&nbsp;&nbsp;Adultos</h4>
+                                <div class="qty-buttons">
+                                    <input type="button" value="-" class="qtyminus" name="adults">
+                                    <input type="text" name="adults" id="adults" value="" class="qty form-control" placeholder="0">
+                                    <input type="button" value="+" class="qtyplus" name="adults">
+                                </div>
+                            </div>
+                            <div id="container_4">
+                                <h4 style="color:white;">&nbsp;&nbsp;&nbsp;&nbsp;Niños</h4>
+                                <div class="qty-buttons">
+                                    <input type="button" value="-" class="qtyminus" name="children">
+                                    <input type="text" name="children" id="children" value="" class="qty form-control" placeholder="0">
+                                    <input type="button" value="+" class="qtyplus" name="children">
+                                </div>
+                            </div>
+                        </div><!-- End group_2 -->
+                        <div id="group_3">
+                            <div id="container_5">
+                                <h4 style="color:white;">&nbsp;&nbsp;&nbsp;&nbsp;Nombre</h4>
+                                <input type="text" class="form-control" name="name_booking" id="name_booking" placeholder="Nombres">
+                            </div>
+                            <div id="container_6">
+                                <h4 style="color:white;">&nbsp;&nbsp;&nbsp;&nbsp;Email</h4>
+                                <input type="text" class="form-control" name="email_booking" id="email_booking" placeholder="email">
+                            </div>
+                        </div><!-- End group_3 -->
+                        <div id="container_7">
+                            <input type="submit" value="Verificar disponibilidad" class="btn_1" id="submit-booking">
+                        </div>
+                    </form>
+                </div><!-- End book_container -->
+            </div><!-- End sub_content -->
+        </div><!-- End subheader -->
+					
+					
+					</figure>
+                </div>
+                <div class="col-md-4 col-md-offset-1">
+                    
+                </div>
+            </div><!-- End row -->
+        </div><!-- End container -->
+    </div><!-- End container_styled_1 -->
+	
+	
+	
+	<br>
+	<br>
+	<hr>
+	
+    <div class="container_styled_1">
+        <h1 class="main_title"><em></em>Bienvenido a HOTEL-RESTAURANTE POZO VIEJO <span>Comodidad y descanso. </span></h1>
+        <p class="lead styled">
+            ¿Qué te parece pasar un fin de semana, fecha especial o vacaciones con nosotros?
+			Nuestra mayor satisfacción será poder atenderte.
+			¡Los esperamos!
+        </p>
+            <div class="col-sm-6 ">
+                <div class="mosaic_container ">
+                    <img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/glorieta.jpg" alt="" class="img-responsive add_bottom_30"><span class="caption_2">Al aire libre</span>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="col-xs-12">
+                    <div class="mosaic_container">
+                        <img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/mesa.jpg" alt="" class="img-responsive add_bottom_30"><span class="caption_2">Sala de Estar</span>
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div class="mosaic_container">
+                        <img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/bano.jpg" alt="" class="img-responsive add_bottom_30"><span class="caption_2">Comodidad</span>
+                    </div>
+                </div>
+                <div class="col-xs-6">
+                    <div class="mosaic_container">
+                        <img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/limonada.jpg" alt="" class="img-responsive add_bottom_30"><span class="caption_2">Delicia</span>
+                    </div>
+                </div>
+            </div>
+        </div><!-- End row -->
+		
+	<br><br><br><br><br><br><br><br><br><br><br><br><br>
+	<hr>
+    
+    <div class="container_styled_1">
+        <div class="container margin_60">
+            <div class="row">
+                <div class="col-md-5 col-md-offset-1">
+                    <figure class="room_pic"><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>#"><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/cama_individual1.jpg" alt="" class="img-responsive"></a>
+					<span class="wow zoomIn"><sup>Desde  </sup>$450<small>Por noche</small></span></figure>
+                </div>
+                <div class="col-md-4 col-md-offset-1">
+                    <div class="room_desc_home">
+                        <h3>Habitaci&oacute;n individual</h3>
+                        <p>
+                             Cuando estes de paso, y deseas descansar, ven y si&eacute;ntete en casa con nuestro servicio.
+                        </p>
+                        <ul>
+                            <li>
+                            <div class="tooltip_styled tooltip-effect-4">
+                                <span class="tooltip-item"><i class="icon_set_2_icon-104"></i></span>
+                                <div class="tooltip-content">
+                                    Cama king size
+                                </div>
+                            </div>
+                            </li>
+                            <li>
+                            <div class="tooltip_styled tooltip-effect-4">
+                                <span class="tooltip-item"><i class="icon_set_2_icon-118"></i></span>
+                                <div class="tooltip-content">
+                                    Regadera
+                                </div>
+                            </div>
+                            </li>
+                            <li>
+                            <?php /*<div class="tooltip_styled tooltip-effect-4">
+                                <span class="tooltip-item"><i class="icon_set_2_icon-116"></i></span>
+                                <div class="tooltip-content">
+                                    Plasma TV
+                                </div>
+                            </div>  */ ?>
+                            </li>
+                        </ul>
+                         <?php /*<a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>#0" class="btn_1_outline">Read more</a> */ ?>
+                    </div><!-- End room_desc_home -->
+                </div>
+            </div><!-- End row -->
+        </div><!-- End container -->
+    </div><!-- End container_styled_1 -->
+    
+    <div class="container_styled_1">
+	<div class="container margin_60">
+        <div class="row">
+            <div class="col-md-5 col-md-offset-1 col-md-push-5">
+                  <figure class="room_pic left"><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>#"><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/cama_doble.jpg" alt="" class="img-responsive"></a>
+				  <span class="wow zoomIn"><sup>Desde </sup>$600<small>Por Noche</small></span></figure>
+            </div>
+            <div class="col-md-4 col-md-offset-1 col-md-pull-6">
+                <div class="room_desc_home">
+                    <h3>Doble Habitaci&oacute;n </h3>
+                    <p>
+                        Nuestros diferentes servicios estan pensados, para cualquier idea que tengas en mente.
+                    </p>
+                    <ul>
+                        <li>
+                        <div class="tooltip_styled tooltip-effect-4">
+                            <span class="tooltip-item"><i class="icon_set_2_icon-104"></i></span>
+                            <div class="tooltip-content">
+                                Cama King size 
+                            </div>
+                        </div>
+                        </li>
+                        <li>
+                        <?php /*<div class="tooltip_styled tooltip-effect-4">
+                            <span class="tooltip-item"><i class="icon_set_2_icon-111"></i></span>
+                            <div class="tooltip-content">
+                                Bathtub
+                            </div>
+                        </div>*/ ?>
+                        </li>
+                        <li>
+						<div class="tooltip_styled tooltip-effect-4">
+							<span class="tooltip-item"><i class="icon_set_2_icon-118"></i></span>
+							<div class="tooltip-content">
+								Regadera
+							</div>
+                        </div>
+                        <div class="tooltip_styled tooltip-effect-4">
+                            <span class="tooltip-item"><i class="icon_set_2_icon-116"></i></span>
+                            <div class="tooltip-content">
+                                Plasma TV
+                            </div>
+                        </div>
+                        </li>
+                        <li>
+                         <?php /*<div class="tooltip_styled tooltip-effect-4">
+                            <span class="tooltip-item"><i class="icon_set_2_icon-106"></i></span>
+                            <div class="tooltip-content">
+                                Safe box
+                            </div>
+                        </div>*/ ?>
+                        </li>
+                    </ul>
+                    <?php /* <a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>#0" class="btn_1_outline">Read more</a> */ ?>
+                </div><!-- End room_desc_home -->
+            </div>
+        </div><!-- End row -->
+    </div><!-- End container -->
+    </div><!-- End container -->
+    
+    <div class="container_styled_1">
+        <div class="container margin_60">
+            <div class="row">
+                <div class="col-md-5 col-md-offset-1">
+                    <figure class="room_pic"><a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>#"><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/varios.jpg" alt="" class="img-responsive"></a><span class="wow zoomIn"><sup>Desde  </sup>$1000<small>Por noche</small></span></figure>
+                </div>
+                <div class="col-md-4 col-md-offset-1">
+                    <div class="room_desc_home">
+                        <h3> En grupo</h3>
+                        <p>
+                             Esta habitación es ideal para un grupo de amigos, ánimate y ven a visitarnos.
+                        </p>
+                        <ul>
+                            <li>
+                            <div class="tooltip_styled tooltip-effect-4">
+                                <span class="tooltip-item"><i class="icon_set_2_icon-104"></i></span>
+                                <div class="tooltip-content">
+                                    King size bed
+                                </div>
+                            </div>
+                            </li>
+                            <li>
+                            <div class="tooltip_styled tooltip-effect-4">
+							<span class="tooltip-item"><i class="icon_set_2_icon-118"></i></span>
+							<div class="tooltip-content">
+								Regadera
+							</div>
+							</div>
+                            </li>
+                            <li>
+                            <div class="tooltip_styled tooltip-effect-4">
+                                <span class="tooltip-item"><i class="icon_set_2_icon-116"></i></span>
+                                <div class="tooltip-content">
+                                    TV
+                                </div>
+                            </div>
+                            </li>
+                            <li>
+                             <?php /*<div class="tooltip_styled tooltip-effect-4">
+                                <span class="tooltip-item"><i class="icon_set_1_icon-15"></i></span>
+                                <div class="tooltip-content">
+                                    Welcome bottle
+                                </div>
+                            </div>*/ ?>
+                            </li>
+                            <li>
+                            <?php /* <div class="tooltip_styled tooltip-effect-4">
+                                <span class="tooltip-item"><i class="icon_set_2_icon-106"></i></span>
+                                <div class="tooltip-content">
+                                    Safe box
+                                </div>
+                            </div> */ ?>
+                            </li>
+                        </ul>
+                        <?php /* <a href="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>#0" class="btn_1_outline">Read more</a> */ ?>
+                    </div><!-- End room_desc_home -->
+                </div>
+            </div><!-- End row -->
+        </div><!-- End container -->
+    </div><!-- End container_styled_1 -->
+    
+    <section class="promo_full">
+    <div class="promo_full_wp">
+        <div>
+            <h3>Comentarios<span>Nos interesa escucharte</span></h3>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="carousel_testimonials">
+                        
+                            <div>
+                                <div class="box_overlay">
+                                    <div class="pic">
+                                        <figure><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/avatar3.jpg" alt="" class="img-circle"></figure>
+                                        <h4>Eustoquia Vazquez Rojas<small>6 Noviembre 2017</small></h4>
+                                    </div>
+                                    <div class="comment">
+                                        "Un lugar excelente con atención de maravilla!!! Me gustó muchísimo. En la noche una elegante iluminación en su centro una fuente hermosa simplemente me fascinó. Valió la pena viajar hasta allá y pasar una excelente noche.."
+                                    </div>
+                                </div><!-- End box_overlay -->
+                            </div>
+                            
+                            <div>
+                                <div class="box_overlay">
+                                    <div class="pic">
+                                        <figure><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/avatar2.jpg" alt="" class="img-circle"></figure>
+                                        <h4>Moisés Montiel Vázquez<small>2 Abril 2015</small></h4>
+                                    </div>
+                                    <div class="comment">
+                                        "Me gusta muchisimo,es un lugar para olvidarse del stress y disfrutar de la naturaleza y de los bellos paisajes de mi bella Zacapoaxtla Puebla"
+                                    </div>
+                                </div><!-- End box_overlay -->
+                            </div>
+                            
+                            <div>
+                                <div class="box_overlay">
+                                    <div class="pic">
+                                        <figure><img src="<?php echo RUTA_ACTUAL.BASE_TEMPLET; ?>img/avatarXD.gif" alt="" class="img-circle"></figure>
+                                        <h4>Ivie Alcántara<small>10 Abril 2017</small></h4>
+                                    </div>
+                                    <div class="comment">
+                                        "Excelente lugar, la comida simplemente deliciosa. Habitaciones confortables e instalaciones de primera. Un lugar en el que sólo se antoja pasarla bien."
+                                    </div>
+                                </div><!-- End box_overlay -->
+                            </div>
+                            
+                        </div><!-- End carousel_testimonials -->
+                    </div><!-- End col-md-8 -->
+                </div><!-- End row -->
+            </div><!-- End container -->
+        </div><!-- End promo_full_wp -->
+    </div><!-- End promo_full -->
+    </section><!-- End section -->    
+    
+    <div id="dtBox"></div><!-- End datepicker -->
+    
