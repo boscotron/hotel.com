db0b445e16ebda6e7b7813c55d61c475cce223c1
Juan Carlos M <Juan Carlos M>
2018-12-15 09:45:06 -0600

    vista de admin

---
+++ templet/adminhotel.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    
+    
+</head>
+<br><br><br><br><br><br><br><br>
+<h1>Hola Mundo</h1>
+<div class="container">
+    <h1 class="conta">hola 1</h1>
+    <h1 class="display-4" id="idh1">hola 2</h1>
+</div>
+<h1 id="jc">Hola Mundo</h1>
+<p class="cambio">Como va todo</p> 
+
+</body>
+</html>
---
+++ templet/js/adminhotel.js
@@ -0,0 +1,51 @@
+
+$(document).ready(function () {
+   $("h1").html("Hola JC");
+    $(".cambio").html("Hola JC Desde Clase");
+    $("#jc").html("id dos");
+    $(".container h1.conta").html("huuula");
+    $("#idh1").addClass("text-danger");
+});
+
+
+/*
+function sumarNumeros(a, b) {
+    let resultado = (a) + (b);     
+    return console.log(resultado);  
+ };
+ sumarNumeros(5, 20);
+
+
+
+ 
+ function multiplicacion(a , b, f){
+     let res = a * b + f;
+     return console.log(res);
+ };
+ multiplicacion(5, 7 , 10);
+
+
+
+
+ 
+ let prueba = function (a, b){
+     let resultado = (a) + (b);     
+    return console.log(resultado);  
+ };
+ prueba(6, 16);
+
+
+
+
+ let resta = new Function("a", "b","return a-b");
+ let resultado = resta(56, 12);
+ console.log(resultado);
+ 
+
+
+
+ function suma(){
+    
+ };
+ suma();
+ */
