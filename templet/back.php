05ac68ab8f589b9d162d43932283ea79a807ef14
CesarCruzGa <43935896+CesarCruzGa@users.noreply.github.com>
2019-01-03 17:33:33 -0600

    back modificado

--- templet/back.php
+++ templet/back.php
@@ -6,10 +6,13 @@ $telefono = $_POST['tel'];
 $mensaje = $_POST['men'];
 
 echo $nombre;
+echo "<br>";
 echo $email;
+echo "<br>";
 echo $telefono;
+echo "<br>";
 echo $mensaje;
-
+echo "<br>";
     echo "Hola tus datos son ";
 
 ?>
