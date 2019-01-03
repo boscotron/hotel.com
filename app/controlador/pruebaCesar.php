735f7c2aa9f70e363dbd2e9c075563a9f2b07594
CesarCruzGa <43935896+CesarCruzGa@users.noreply.github.com>
2019-01-03 16:58:11 -0600

    prueba_formulario
    formulario

---
+++ app/controlador/pruebaCesar.php
@@ -0,0 +1,10 @@
+<?php 
+
+$jmyWeb ->cargar(["pagina"=>"pruebaCesar.php"]);
+$jmyWeb->cargar_js(["url"=>$jmyWeb ->url_templet(["return"=>true])."js/pruebaCesar.js"]);
+$jmyWeb ->cargar_vista(["url"=>"pruebaCesar.php"]);
+
+
+
+
+ ?>
