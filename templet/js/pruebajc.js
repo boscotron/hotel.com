715ff8b2ed822a2d86335030fae17b22e4576f53
Juan Carlos M <Juan Carlos M>
2019-01-13 16:24:30 -0600

    clase Anna

--- templet/js/pruebajc.js
+++ templet/js/pruebajc.js
@@ -1,50 +1,15 @@
 $(document).ready(function () {
-console.log("desde js");
- /*            
-$('#enviar').click(function () {
-    console.log("desde funcion");
-    alert("tengo input " + $("input[name='nombre']","#nombre").length + " inputs");
-
-    
-    var nombre = document.getElementById('nombre').value;
-    var email = document.getElementById('email').value;
-    var telefono = document.getElementById('telefono').value;
-    var mensaje = document.getElementById('mensaje').value;
-    
+            
    
-    var ruta = "&nom="+nombre+"&ema="+email+"&tel="+telefono+"&men="
-+mensaje;
-
-    $.ajax({
-        url:location.origin + '/templet/back.php',
-        type:'post',
-        data:ruta,
-    })
-    .done(function (res) {
-        $("#respuesta").html(res)
-    })
-
-});
-
-$('#borrar').click(function () {
-    $('#respuesta').empty();
-
-});
+    $('#enviar').click(
+        function () {
+        alert("me dieron click " );
+    
+       
 
 
-$('#sumar').click(function () {
-    var num1 = document.getElementById('num1').value;
-    var num2 = document.getElementById('num2').value;
-    $("input[name='nombre']").val("Juan");
-    var num1 = Number(num1);
-    var num2 = Number(num2);
-    var resultado = num1+num2;   
-    $('#respuesta').wrap("<div class='row justify-content-center alert alert-success' role='alert'><div class='col-4'> </div></div>").append("<br>La suma de "+num1+" + "+num2+" es "+resultado);
-
-});
- */
-
-
+    });
+ 
 });
 
 
