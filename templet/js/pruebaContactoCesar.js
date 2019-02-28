b7438195ca0431b5794ff7cad693dadb38738abf
CesarCruzGa <43935896+CesarCruzGa@users.noreply.github.com>
2019-02-28 17:08:41 -0600

    PruebaContacto
    modificado

--- templet/js/pruebaContactoCesar.js
+++ templet/js/pruebaContactoCesar.js
@@ -4,23 +4,22 @@ $(document).ready(function(){
 	$('#enviarC').click(function(){
 		var nombre = document.getElementById('nombre').value;
 		var email = document.getElementById('email').value;
-		var telefono = document.getElementById('telefono');
-		var telefono1 = document.getElementById('telefono1');
-		var mensaje = document.getElementById('mensaje');
+		var telefono = document.getElementById('telefono').value;
+		var telefono1 = document.getElementById('telefono1').value;
+		var mensage = document.getElementById('mensage').value;
 
-		alert("Tus datos han sido guardados con exito: "+"\n" +nombre+"\n"+email+
-			"\n"+telefono+
-			"\n"+telefono1+			
-			"\n"+mensaje);
-console.log("Funcion borrar");
-	$('#nombre').empty('nombre');
+		alert("Tus datos han sido guardados con exito: "+"\n" +nombre+"\n"+email+"\n"+telefono+"\n"+telefono1+"\n"+mensage);
+
+
+		console.log("Funcion borrar");
+		$('#nombre').empty('nombre');
 			
 
 		/*$ajax({
 			url:location.origin + '/pruebaContactoCesar1ws',
    			type:'POST',
     		dateType: 'json',
-    		data: {nombre:nombre, email:email, telefono:telefono, mensaje:mensaje},
+    		data: {nombre:nombre, email:email, telefono:telefono, mensage:mensage},
 
 		});*/
 
